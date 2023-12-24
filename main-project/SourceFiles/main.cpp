cc6e93c1bcadde1b14eee3ae009ac36a8d5f6b21
Daniil4634 <154693506+Daniil4634@users.noreply.github.com>
2023-12-24 12:52:24 +0300

    Update main.cpp
    Добавлены тестовые данные

--- main-project/SourceFiles/main.cpp
+++ main-project/SourceFiles/main.cpp
@@ -5,7 +5,8 @@ using namespace std;
 int main()
 {
     cout << "Laboratory work #8. GIT\n";
-    cout << "Variant #0. Library Subscription\n";
+    cout << "Variant #1. Library Subscription\n";
     cout << "Author: Valentinovitch Daniil\n";
+    cout << "Group: 23Ping1d\n";
     return 0;
 }
